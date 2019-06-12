1543dc25987c16de92c80280ef531640ecf8e200
Danny-Tran <truong_tran@live.com>
2019-06-12 18:25:11 +0000

    complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+    belongs_to :store
+    validates :first_name, presence:true
+    validates :last_name, presence:true
+    # validates :store_id
+    validates :hourly_rate, :inclusion => {:in => [40,200], :only_integer => true}
+    validates :annual_revenue, :inclusion => {in: [0], :only_integer => true}
+    
+end
+
+@store2.employees.create(first_name:"Peter" , last_name: "Syn", hourly_rate:45 )
